e89f763eeca0877cb34ecb7c6645ebb2867fd886
unknown <wilfriedngassarou23@gmail.com>
2025-03-14 00:41:45 +0100

    V2 (with gradients)

--- src/App.tsx
+++ src/App.tsx
@@ -19,16 +19,46 @@ function App() {
     setActiveCarIndex(activeCarIndex - 1)
   }
 
-
   return (
     <section className="w-full h-dvh flex justify-center items-center">
-      <div className="w-full md:w-80 h-full md:h-5/6 lg:border-2 flex flex-col overflow-hidden">
+      <div className="w-full md:w-80 min-w-80 max-w-[400px] h-full md:h-5/6 min-h-[590px] max-h-[700px] lg:border-2 flex flex-col overflow-hidden">
         <section className="h-1/2 w-full relative">
-          {/* <div className="w-[300%] h-full flex bg-red-300">
-            <div className="flex-grow bg-blue-200"></div>
-            <div className="flex-grow bg-orange-200"></div>
-            <div className="flex-grow bg-lime-200"></div>
-          </div> */}
+          <div 
+            className={clsx(
+              "w-[1179px] h-full flex duration-500 ease-in-out",
+              activeCarIndex == 0 && "-translate-x-0 -translate-y-0",
+              activeCarIndex == 1 && "-translate-x-1/3 -translate-y-24",
+              activeCarIndex == 2 && "-translate-x-2/3 -translate-y-0",
+            )}
+          >
+            <svg
+              width="1179"
+              height="466"
+              className="relative -top-16"
+              fill="none"
+            >
+              <path
+                stroke="url(#paint0_linear_3_70)"
+                strokeLinecap="round"
+                strokeWidth="256.528"
+                d="M-23.5 285.5c208.109-74.556-46.22 15.227 161.888-59.328 208.11-74.556 335.822 137.463 554.77 108.985 226.119-29.41 296.743-227.325 522.242-261.128"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_3_70"
+                  x1="-397.954"
+                  x2="1791.96"
+                  y1="392.691"
+                  y2="83.337"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#C1810A"></stop>
+                  <stop offset="0.515" stopColor="#910398"></stop>
+                  <stop offset="1" stopColor="#14C8BF"></stop>
+                </linearGradient>
+              </defs>
+            </svg>
+          </div>
           <section className="absolute inset-0 flex justify-center items-center">
             {cars.map((item, index) => (
               <div
@@ -49,9 +79,7 @@ function App() {
             ))}
           </section>
         </section>
-        <section 
-          className="h-1/2 w-full flex flex-col gap-6 px-4"
-        >
+        <section className="h-1/2 w-full flex flex-col justify-end py-6 gap-6 px-4">
           <h2 className="text-center relative flex justify-center items-center font-semibold text-xl">
             {cars.map((item, index) => (
               <span 
